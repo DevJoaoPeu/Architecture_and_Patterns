6c1c8279f8098a46d2a3da4119b0c85c37538d6c
DevJoaoPeu <joaocanaldroid@gmail.com>
2024-12-01 15:46:37 -0300

    feat: configs to database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,26 @@
 import { Module } from '@nestjs/common';
 import { UploadModule } from './upload/upload.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UploadModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      database: process.env.DB_DATABASE,
+      host: process.env.DB_HOST,
+      password: process.env.DB_PASSWORD,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+      ssl: false,
+    }),
+    UploadModule],
   controllers: [],
   providers: [],
 })
