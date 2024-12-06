f2ff6f93e3d75a9af92f6045bd72badb4d062886
DevJoaoPeu <joaocanaldroid@gmail.com>
2024-12-06 16:14:49 -0300

    feat: adding test to upload-controller

--- src/upload/test/upload.controller.spec.ts
+++ src/upload/test/upload.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UploadServiceInterface } from "../interface/upload.service.interface"
 import { UploadController } from '../upload.controller';
+import { BadRequestException } from '@nestjs/common';
 
 describe('UploadController', () => {
     let uploadController: UploadController;
@@ -26,4 +27,36 @@ describe('UploadController', () => {
     it('should be defined', () => {
         expect(uploadController).toBeDefined();
     });
+
+    describe('uploadXlsx', () => {
+        it('deve retornar um error caso o arquivo nao seja enviado', () => {
+            expect(() => uploadController.uploadXlsx(undefined))
+                .toThrow(new BadRequestException('Arquivo é obrigatório'));
+        });
+
+        it('deve ser definido', async () => {
+            const mockFile: Express.Multer.File = {
+                fieldname: 'file',
+                originalname: 'mock-file.xlsx',
+                encoding: '7bit',
+                mimetype: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+                buffer: Buffer.from('mock Excel data'),
+                size: 12345,
+                stream: null, 
+                destination: '',
+                filename: 'mock-file.xlsx',
+                path: '',
+            };
+
+            (uploadService.uploadXlsx as jest.Mock).mockResolvedValue({success: true, message: 'Arquivo inserido com sucesso'});
+
+            const result = await uploadController.uploadXlsx(mockFile);
+
+            expect(uploadService.uploadXlsx).toHaveBeenCalledWith(mockFile);
+            expect(result).toEqual({
+                success: true,
+                message: 'Arquivo inserido com sucesso',
+            });
+        });
+    })
 })
