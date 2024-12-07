f1d000249c7667972145d398131781e9ec9291cd
DevJoaoPeu <joaocanaldroid@gmail.com>
2024-12-07 11:23:38 -0300

    test: creted test to user controller

--- src/user/test/user.controller.spec.ts
+++ src/user/test/user.controller.spec.ts
@@ -27,4 +27,30 @@ describe('UploadController', () => {
     it('should be defined', () => {
         expect(userController).toBeDefined();
     });
+
+    describe('create', () => {        
+        it('deve criar um novo usuário', async () => {
+            const user = { name: 'John Doe', email: 'QK2tM@example.com', surname: 'Doe', createdAt: new Date(), updatedAt: new Date() }
+            const result = await userController.create(user);
+
+            jest.spyOn(userService, 'createMany').mockResolvedValue();
+            
+            expect(result).toEqual(undefined);
+        });
+    })
+
+    describe('createMany', () => {
+        it('deve criar vários usuários', async () => {
+            const users = [
+                { name: 'John Doe', email: 'QK2tM@example.com', surname: 'Doe', createdAt: new Date(), updatedAt: new Date() },
+                { name: 'John Doe', email: 'kajskd@example.com', surname: 'Doe', createdAt: new Date(), updatedAt: new Date() }
+            ]
+
+            jest.spyOn(userService, 'createMany').mockResolvedValue();
+
+            const result = await userController.createMany(users);
+            expect(userService.createMany).toHaveBeenCalledWith(users);
+            expect(result).toEqual(undefined);
+        });
+    })
 })
